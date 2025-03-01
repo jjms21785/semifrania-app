e52aada0557d1af048985abfd2db6ae3aaa302ea
Joshua <semifrania_joshuajehiel@plpasig.edu.ph>
2025-03-01 18:16:08 +0800

    Exercise#3 - Routing, Middleware and CSRF

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,53 @@ Route::get('/test4',function (UserService $userService) {
     return Response::json($userService->listUsers());
 });
 
+//test-route
+Route::get('/test-route/{post}/comment/{comment}', function (string $postID, string $comment) {
+    return "Post ID: $postID, Comment: $comment";
+});
+
+Route::get('/post/{id}', function (string $id) {
+    return $id;
+})->where('id', '[0-9]+');
+
+Route::get('/search/{search}', function (string $search) {
+    return $search;
+})->where('search', '.*');
+
+Route::get('/test/route', function () {
+    return route ('test-route');
+})->name('test-route');
+
+Route::middleware(['user-middleware'])->group(function (){
+    Route::get('/route-middleware-group/first', function (Request $request){
+        echo "First";
+    });
+    Route::get('/route-middleware-group/second', function (Request $request){
+        echo "Second";
+    });
+});
+
+// Route - controller
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/users', 'index');
+    Route::get('/users/first', 'first');
+    Route::get('/users/{id}', 'show');
+});
+
+//CSRF
+/*
+Route::get('/token', function (Request $request) {
+    $token = $request->session()->token();
+    return view('token', ['token' => $token]);
+});
+*/
+
+Route::get('/token', function (Request $request) {
+    $token = $request->session()->token();
+    return view('token');
+});
+
+Route::post('/token', function (Request $request) {
+    return $request->all();
+});
